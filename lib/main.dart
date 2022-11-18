fd9b9a312c3b0a4eb6be14a2ae7c5ff5c708a543
gani2267 <prathameshpatil2267@gmail.com>
2022-11-19 00:00:14 +0530

    patil1

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        fontFamily: 'ibmPlexSans',
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        dividerColor: grey,
+        splashColor: grey,
+        inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(6),
+              borderSide: BorderSide(color: grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+                borderSide: BorderSide(color: almostWhite)),
+            floatingLabelBehavior: FloatingLabelBehavior.auto,
+            focusColor: almostWhite,
+            labelStyle: bodyText2White60),
       ),
       home: const SplashScreen(),
     );
